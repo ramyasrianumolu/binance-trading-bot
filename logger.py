f6546582aaf11ef86ffa3182061d9a3666e9633c
ramyasrianumolu <ramyasrianumolu@gmail.com>
2026-05-01 21:37:43 +0530

    Add logging setup

---
+++ logger.py
@@ -0,0 +1,14 @@
+import logging
+
+def setup_logger():
+    logger = logging.getLogger("trading_bot")
+    logger.setLevel(logging.INFO)
+
+    file_handler = logging.FileHandler("bot.log")
+    formatter = logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s"
+    )
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    return logger
