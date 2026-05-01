264cd6488f4a6d5db5f1e525cf469f05a9a5c1ac
ramyasrianumolu <ramyasrianumolu@gmail.com>
2026-05-01 21:36:32 +0530

    Add configuration file

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("BINANCE_API_KEY")
+API_SECRET = os.getenv("BINANCE_API_SECRET")
+
+BASE_URL = "https://testnet.binancefuture.com"
